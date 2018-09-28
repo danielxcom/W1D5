8a0954ce29c0d9915c25edb66d44302d8fbeecad
danielxcom <danixu12@gmail.com>
2018-09-28 16:48:53 -0400

    added add_child and completed the initialize

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,13 +1,32 @@
 class PolyTreeNode
-  def initialize(value)
+  attr_accessor :children, :value, :parent
+
+  def initialize(children, value, parent = nil)
+    @value = value
+    # @parent = []
+    @parent = parent
+    @children = []
+  end
+
+  def parent = (parent_param)
+    @parent.children.delete(self) unless @parent == nil
+    @parent = parent_param
+    unless @parent == nil || @parent.children.include?(self)
+      @parent.children << self
+    end
 
   end
-  def parent
 
-  end
+  def add_children(c_node)
+    if @children.include?(c_node)
+      c_node.parent = nil
+      @children.delete(c_node)
 
-  def children
-
+    elsif @parent == c_node
+      raise "Parent is designated. Cannot remove child from parent."
+    else
+      raise "Child not found."
+    end
   end
 
   def value
